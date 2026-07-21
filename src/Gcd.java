d606a2925687186c89d3f61d02ba113937418521
vikash <expertvikash@gmail.com>
2026-07-21 11:12:40 +0530

    gdc java

--- src/Gcd.java
+++ src/Gcd.java
@@ -1,3 +1,14 @@
-public class Gcd {
 
+//here GCD sum
+//using gcd(a,b) = gcd(b,a%b) => end = gcd(a,0) => RETURN a
+public static void main(String arg[]){
+    int a=18; int b=12;
+    while(b!=0){
+        int temp = b;
+        b= a%b;
+        a = temp;
+
+        System.out.println("a="+a + " b=" + b);
+    }
+    System.out.println("final ans = " +a);
 }
